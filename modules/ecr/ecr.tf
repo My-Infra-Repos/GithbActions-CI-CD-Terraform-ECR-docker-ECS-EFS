7b295e9c86bdc95660ebd7e72a53ffc9d889a178
unknown <praveen1664@outlook.com>
2022-08-09 12:35:36 +0530

    First Commit

---
+++ modules/ecr/ecr.tf
@@ -0,0 +1,10 @@
+
+resource "aws_ecr_repository" "image_registry" {
+  name                 = "${var.ecr_name}-${var.env}"
+  image_tag_mutability = var.image_tag_mutability
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
