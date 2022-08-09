8c703eb291100eaf60ee0cf49e838bb832035375
unknown <praveen1664@outlook.com>
2022-08-09 14:29:28 +0530

    TF_vars Updated

--- environment/demo/provider.tf
+++ environment/demo/provider.tf
@@ -10,14 +10,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-<<<<<<< HEAD
   profile = "default"
   access_key = var.access_key
   secret_key = var.secret_key
 
-=======
-  #access_key=  secrets.AWS_ACCESS_KEY_ID
-  #secret_key = secrets.AWS_SECRET_ACCESS_KEY
-#   profile = "default"
->>>>>>> 2f2d1acf93297bfcf7c7d5c3d0f4cc8da6a5cda6
 }
