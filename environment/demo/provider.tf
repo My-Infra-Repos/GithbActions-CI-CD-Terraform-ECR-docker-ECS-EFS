ca81169be0e15111475e767b6efdc9e082ed39af
Praveen <36715149+praveen1664@users.noreply.github.com>
2022-08-09 14:11:14 +0530

    Update provider.tf

--- environment/demo/provider.tf
+++ environment/demo/provider.tf
@@ -10,5 +10,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
+  access_key=  secrets.AWS_ACCESS_KEY_ID
+  secret_key = secrets.AWS_SECRET_ACCESS_KEY
 #   profile = "default"
 }
